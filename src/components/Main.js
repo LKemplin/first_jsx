f5135b64bf009508106865df007d3af9ec654e98
Lynsey Kemplin <purplez479@gmail.com>
2022-07-10 14:45:11 -0700

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+
+
+const Main = () => {
+
+    return (
+        <div>
+            <h1>Hello Dojo!</h1>
+            <h2>Things I Need to Do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Practice Javascript</li>
+                    <li>Clean the litterboxes</li>
+                    <li>Take out the garbage</li>
+                </ul>
+        </div>
+
+    )
+}
+
+export default Main
